8f241e3f827ffbb4743d65e756f9894f8ce840ce
TarasKvyk <tasik981@gmail.com>
2024-11-17 15:01:36 +0200

    Added fileds validation

--- frontend/railway/src/app/components/start-page/start-page.component.ts
+++ frontend/railway/src/app/components/start-page/start-page.component.ts
@@ -1,7 +1,6 @@
 import {Component} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
-import {StationConfiguration} from '../../models/station-configuration';
 import {CustomerGenerationType} from '../../models/enums/customer-generation';
 
 @Component({
@@ -16,14 +15,18 @@ export class StartPageComponent {
     cashDeskCount: null as number | null,
     entranceCount: null as number | null,
     serviceTime: null as number | null,
-    generationType: null
+    generationType: CustomerGenerationType.Simple
   };
 
-  start(): void {
-    console.log('Start button clicked!');
-    console.log('Number of Ticket Offices:', this.stationConfiguration.cashDeskCount);
-    console.log('Number of Entrances:', this.stationConfiguration.entranceCount);
-    console.log('Service Time:', this.stationConfiguration.serviceTime);
-    console.log('Customer Acquisition Strategy:', this.stationConfiguration.generationType);
+  Submit(form: any): void {
+    if (form.valid) {
+      console.log('Start button clicked!');
+      console.log('Number of Ticket Offices:', this.stationConfiguration.cashDeskCount);
+      console.log('Number of Entrances:', this.stationConfiguration.entranceCount);
+      console.log('Service Time:', this.stationConfiguration.serviceTime);
+      console.log('Customer Acquisition Strategy:', this.stationConfiguration.generationType);
+    } else {
+      console.log('Form is invalid');
+    }
   }
 }
