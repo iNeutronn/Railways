4b0bc2d33299e7c786b3bf5d518f8e1c191afe7c
Roman <roman.marushko.pz.2022@lpnu.ua>
2024-11-18 18:22:54 +0200

    HallSimulator added

--- src/main/java/com/railways/railways/simulator/src/hall/HallSimulator.java
+++ src/main/java/com/railways/railways/simulator/src/hall/HallSimulator.java
@@ -1,4 +1,16 @@
 package com.railways.railways.simulator.src.hall;
 
 public class HallSimulator {
+    private TicketOfficeSimulator ticketOfficeSimulator;
+    private IGeneration<?> clientGenerator;
+
+    public HallSimulator(TicketOfficeSimulator ticketOfficeSimulator, IGeneration<?> clientGenerator) {
+        this.ticketOfficeSimulator = ticketOfficeSimulator;
+        this.clientGenerator = clientGenerator;
+    }
+
+    public void run() {
+        ticketOfficeSimulator.run();
+        clientGenerator.Generate();
+    }
 }
