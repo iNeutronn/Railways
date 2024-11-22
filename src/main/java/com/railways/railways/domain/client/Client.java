65597ad3f02e05c80966ba5a03737453006d5b56
Ivan Misarosh <ivan.misarosh05@gmail.com>
2024-11-22 18:53:10 +0200

    setting privilege in constructor

--- src/main/java/com/railways/railways/domain/client/Client.java
+++ src/main/java/com/railways/railways/domain/client/Client.java
@@ -11,13 +11,14 @@ public class Client {
     private Point position;
     private final long timestamp; // New field to store insertion time
 
-    public Client(int clientID, String firstName, String lastName, int ticketsToBuy, Point position) {
+    public Client(int clientID, String firstName, String lastName, int ticketsToBuy, Point position, PrivilegeEnum privilege) {
         this.clientID = clientID;
         this.firstName = firstName;
         this.lastName = lastName;
         this.ticketsToBuy = ticketsToBuy;
         this.position = position;
         this.timestamp = System.currentTimeMillis();
+        this.privilege = privilege;
     }
 
     public long getTimestamp() {
@@ -56,4 +57,15 @@ public class Client {
         return position;
     }
 
+    public void setPrivilege(PrivilegeEnum privilege) {
+        this.privilege = privilege;
+    }
+
+    public void setTicketsToBuy(int ticketsToBuy) {
+        this.ticketsToBuy = ticketsToBuy;
+    }
+
+    public void setPosition(Point position) {
+        this.position = position;
+    }
 }
