791a70c5a5b55687a9f35ae394eefd5acb7e42da
Roman <roman.marushko.pz.2022@lpnu.ua>
2024-11-23 00:14:29 +0200

    rename client moved event

--- src/main/java/com/railways/railways/events/ClientMovedEvent.java
+++ src/main/java/com/railways/railways/events/ClientMovedEvent.java
@@ -3,10 +3,10 @@ package com.railways.railways.events;
 import com.railways.railways.domain.client.Client;
 import org.springframework.context.ApplicationEvent;
 
-public class ClientUpdatedEvent extends ApplicationEvent {
+public class ClientMovedEvent extends ApplicationEvent {
     private final Client client;
 
-    public ClientUpdatedEvent(Object source, Client client) {
+    public ClientMovedEvent(Object source, Client client) {
         super(source);
         this.client = client;
     }
