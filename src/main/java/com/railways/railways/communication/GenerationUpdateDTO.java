aac162df500c8d55fff631f17ee3a98c4d4440a7
iNeutronn <57719073+iNeutronn@users.noreply.github.com>
2024-11-23 11:09:14 +0200

    now GenerationUpdateDTO is not interface, it's an abstract class

--- src/main/java/com/railways/railways/communication/GenerationUpdateDTO.java
+++ src/main/java/com/railways/railways/communication/GenerationUpdateDTO.java
@@ -1,22 +1,32 @@
 package com.railways.railways.communication;
 
+import java.io.Serializable;
+
 /**
- * GenerationUpdateDTO is an interface that represents an update to be sent to clients.
- * It provides methods to get the type of the update and the data associated with the update.
+ * GenerationUpdateDTO is an abstract class that represents an update to be sent to clients.
+ * It contains public fields for the type of the update and the data associated with it.
+ * This structure ensures proper serialization and deserialization.
  */
-public interface GenerationUpdateDTO {
+public abstract class GenerationUpdateDTO implements Serializable {
 
     /**
-     * Gets the type of the update.
-     *
-     * @return the type of the update
+     * The type of the update.
      */
-    GenerationUpdateTypes getType();
+    public GenerationUpdateTypes type;
 
     /**
-     * Gets the data associated with the update.
-     *
-     * @return the data associated with the update
+     * The data associated with the update.
      */
-    Object getData();
+    public Object data;
+
+    /**
+     * Constructor for creating a GenerationUpdateDTO.
+     *
+     * @param type the type of the update
+     * @param data the data associated with the update
+     */
+    public GenerationUpdateDTO(GenerationUpdateTypes type, Object data) {
+        this.type = type;
+        this.data = data;
+    }
 }
