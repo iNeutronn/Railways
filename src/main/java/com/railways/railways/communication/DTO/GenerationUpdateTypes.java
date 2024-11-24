0db7162865b210280bdbb1d2b168c1a7d2425f57
Ivan Misarosh <ivan.misarosh05@gmail.com>
2024-11-24 18:34:11 +0200

    removed unused event

--- src/main/java/com/railways/railways/communication/DTO/GenerationUpdateTypes.java
+++ src/main/java/com/railways/railways/communication/DTO/GenerationUpdateTypes.java
@@ -2,7 +2,6 @@ package com.railways.railways.communication.DTO;
 
 public enum GenerationUpdateTypes {
     ClientCreated,
-    ClientMoved,
     ClientServed,
     QueueUpdated
     //add more types here
