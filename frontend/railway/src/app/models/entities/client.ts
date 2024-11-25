981baf3846cdf390070834db12ade0ec7c832579
TarasKvyk <tasik981@gmail.com>
2024-11-25 02:40:40 +0200

    Changed client entity and added PrivilegeEnumLabels to convert into css class later

--- frontend/railway/src/app/models/entities/client.ts
+++ frontend/railway/src/app/models/entities/client.ts
@@ -1,8 +1,10 @@
 import {PrivilegeEnum} from '../enums/privilege-enum';
 import {Ticket} from './ticket';
+import {Position} from '../position';
 
 export interface Client {
   clientID: number;
+  position: Position;
   firstName: string;
   lastName: string;
   ticketsToBuy: number;
--- frontend/railway/src/app/models/enums/privilege-enum.ts
+++ frontend/railway/src/app/models/enums/privilege-enum.ts
@@ -4,3 +4,10 @@ export enum PrivilegeEnum {
   WARVETERAN,
   WITHCHILD,
 }
+
+export const PrivilegeEnumLabels = {
+  0: 'default',
+  1: 'disabled',
+  2: 'warveteran',
+  3: 'withchild',
+};
